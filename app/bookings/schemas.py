f7cd98c87f0804e5634e0fba3e0d2c273f9eef6a
Luna <kottakcode@gmail.com>
2026-03-16 22:07:08 +0300

    feat(booking) schemas

---
+++ app/bookings/schemas.py
@@ -0,0 +1,37 @@
+
+from pydantic import BaseModel, Field
+
+from app.availabilities.schemas import AvailabilityUserReservationViewSchema
+from app.users.schemas import UserDataFromDbSchema
+
+
+
+# ============================= #
+# ==== FORMS ================== #
+# ============================= # 
+
+# TAKE A RDV (BOOKING) =================================
+class TakeBookingByPatientFormSchema(BaseModel):
+
+    availability_id: int
+
+    message_to_practitioner: str = Field(
+        min_length=5,
+        max_length=800,
+        description="Message <string>: 5 to 800 chars.",
+        default=None,
+    )
+
+
+# ============================= #
+# ==== READS ================== #
+# ============================= #
+
+# ========= PRACTITIONER SEE PATIENT BOOKING ===============
+class PatientBookingDataForPractitionerSchema(BaseModel):
+
+    model_config={"from_attributes":True}
+
+    patient: UserDataFromDbSchema
+    availability: AvailabilityUserReservationViewSchema
+    message_to_practitioner: str
