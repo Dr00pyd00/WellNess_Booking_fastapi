b856d26743d9f0bd71cd6d125e26b91a92d03942
Luna <kottakcode@gmail.com>
2026-03-19 12:08:22 +0300

    feat/fix(main+models) charging all models at the start for models creation + add router users

---
+++ app/all_models.py
@@ -0,0 +1,4 @@
+from app.users.models import User
+from app.practitioners.models import Practitioner
+from app.availabilities.models import Availability
+from app.bookings.models import Booking
--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 
 from app.core.config import app_settings
+from app import all_models
+
+# routers imports:
+from app.users.router import router as users_router
 
 
 app = FastAPI()
+
+# Routers Bindings:
+app.include_router(users_router)
 
 
 @app.get("/")
