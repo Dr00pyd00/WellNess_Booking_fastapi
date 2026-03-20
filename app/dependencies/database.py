8ad498abf832ce6e3843a426e320897efb8feccf
Luna <kottakcode@gmail.com>
2026-03-20 21:53:00 +0300

    fix(get_db) was sync i make it async

--- app/dependencies/database.py
+++ app/dependencies/database.py
@@ -1,19 +1,19 @@
 from app.core.database import LocalSession
 
 
-# si sync: 
-def get_db():
-    db = LocalSession()
-    try:
-        yield db
-    finally:
-        db.close()
+# # si sync: 
+# def get_db():
+#     db = LocalSession()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 
         
 # si async:
     # comme pour ouvrir un fichier text par exemple avec with: le with va fermer correctement:
-async def async_get_db():
+async def get_db():
     async with LocalSession() as db:
         yield db 
 
