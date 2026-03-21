3ee695347360899c6b7a6b34513dfa5b6d9086b7
Luna <kottakcode@gmail.com>
2026-03-21 21:28:06 +0300

    fix(users) exceptions try deleted last admin error message now 403 forbiden

--- app/users/exceptions.py
+++ app/users/exceptions.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, status
 
 def try_soft_delete_last_admin_error_msg():
     raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
+        status_code=status.HTTP_403_FORBIDDEN,
         detail=f"You can't delete last admin"
     )
 
