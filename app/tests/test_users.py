05a0cbb31272ee5d7c5dc6a2826f6ecff48d2b73
Luna <kottakcode@gmail.com>
2026-03-24 11:15:44 +0300

    feat(tests) swap role/status/soft_delete

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -1,7 +1,9 @@
 
 from httpx import AsyncClient, Response
 
+from app.core.models_mixins.mixin_status import StatusEnum
 from app.users.models import UserRoleEnum
+from app.users.models import User
 
 
 # ==================================================================================== #
@@ -211,6 +213,19 @@ async def test_get_me_success(client: AsyncClient, create_user_response: Respons
     assert "\"username\":\"usernametest\"" in response.text
 
 
+async def test_get_me_admin_success(client: AsyncClient, get_admin_user_token):
+
+    response = await client.get(url="users/me",
+                          headers={
+                              "Authorization":f"Bearer {get_admin_user_token}"
+                          })
+    
+    assert response.status_code == 200
+    assert response.json()["role"] == UserRoleEnum.ADMIN.value
+
+
+
+
 async def test_get_me_fail_bad_token(client: AsyncClient, create_user_response: Response, get_created_user_token):
 
     assert create_user_response.status_code == 201
@@ -237,4 +252,216 @@ async def test_get_me_fail_no_token(client: AsyncClient, create_user_response: R
 
     assert response.status_code == 401
     assert "Not authenticated" in response.text
+
+
+# ==================================================================================== #
+#                  UPDATE USER PROFILE
+# ==================================================================================== #
+
+async def test_update_user_success(client: AsyncClient,create_user_response: Response, get_created_user_token:str ):
+
+    data_to_update = {"username":"UPDATEDusername"}
+
+    response = await client.patch("/users/me", 
+                            json=data_to_update, 
+                            headers={"Authorization":f"Bearer {get_created_user_token}"
+                            })
+    
+    assert create_user_response.status_code == 201
+    assert response.status_code == 200
+    assert response.json()["username"] == "UPDATEDusername"
+    assert response.json()["id"] == create_user_response.json()["id"]
+
+
+async def test_update_user_fail_no_token(client: AsyncClient,create_user_response: Response ):
+
+    data_to_update = {"username":"UPDATEDusername"}
+
+    response = await client.patch("/users/me", 
+                            json=data_to_update, 
+                            )
+    
+    assert create_user_response.status_code == 201
+    assert response.status_code == 401
+    assert "Not authenticated" in response.text
+
+
+
+# ==================================================================================== #
+#                  UPDATE USER PASSWORD
+# ==================================================================================== #
+
+async def test_update_user_password_success(client: AsyncClient, create_user_response: Response, get_created_user_token:str):
+
+    assert create_user_response.status_code == 201
+
+    response = await client.patch(url="users/me/password",
+                 json={
+                     "old_password": user_data_creation["password"],
+                     "new_password": "UPDATEDpassword123"
+                 },
+                 headers={
+                     "Authorization":f"Bearer {get_created_user_token}"
+                 })
+    
+
+    assert response.status_code == 200
+    assert response.json()["id"] == create_user_response.json()["id"]
+
+
+
+    
+
+async def test_update_user_password_fail_no_token(client: AsyncClient, create_user_response: Response):
+
+    assert create_user_response.status_code == 201
+
+    response = await client.patch(url="users/me/password",
+                 json={
+                     "old_password": user_data_creation["password"],
+                     "new_password": "UPDATEDpassword123"
+                 },)
+    
+
+    assert response.status_code == 401
+    assert "Not authenticated" in response.text
+
+
+    
+
+# ==================================================================================== #
+#                  SOFT DELETE USER
+# ==================================================================================== #
+
+async def test_soft_delete_user_success(client: AsyncClient, create_user_response: Response, get_created_user_token:str, get_user_object):
+
+    assert create_user_response.status_code == 201
+    assert get_user_object.deleted_at == None
+
+    response = await client.delete(url="users/me",
+                                   headers={"Authorization":f"Bearer {get_created_user_token}"}
+                                   )
+    
+    assert response.status_code == 200
+    assert response.json()["deleted_at"] != None
+    
+
+
+async def test_soft_delete_user_fail_no_token(client: AsyncClient, create_user_response: Response, get_created_user_token:str, get_user_object):
+
+    assert create_user_response.status_code == 201
+    assert get_user_object.deleted_at == None
+
+    response = await client.delete(url="users/me",
+                                   )
+    
+    assert response.status_code == 401
+    assert "Not authenticated" in response.text
+
+
+async def test_soft_delete_fail_last_admin(client: AsyncClient,get_admin_user: User, get_admin_user_token: str):
+
+    assert get_admin_user.role == UserRoleEnum.ADMIN
+    assert get_admin_user.deleted_at == None
+
+    response = await client.delete("users/me",
+                                   headers={"Authorization":f"Bearer {get_admin_user_token} "})
+
+    assert response.status_code == 403
+    assert "can\'t delete last admin" in response.text
+
+
+
+# ==================================================================================== #
+#                  SWAP ROLE
+# ==================================================================================== #
+
+async def test_swap_role_success(client: AsyncClient, get_admin_user_token:str, get_user_object: User):
+
+    assert get_user_object.role == UserRoleEnum.PATIENT # default value
+
+    response = await client.patch(url=f"users/{get_user_object.id}/role",
+                                  json={"new_role": str(UserRoleEnum.PRACTITIONER.value)},
+                                  headers={"Authorization":f"Bearer {get_admin_user_token}"}
+                                    )
+
+    assert response.status_code == 200
+    assert response.json()["role"] == UserRoleEnum.PRACTITIONER.value
+
+
+
+async def test_swap_role_fail_no_admin(client: AsyncClient, get_created_user_token: str, get_user_object: User):
+
+    assert get_user_object.role == UserRoleEnum.PATIENT # default value
+
+    response = await client.patch(url=f"users/{get_user_object.id}/role",
+                                  json={"new_role": str(UserRoleEnum.PRACTITIONER.value)},
+                                  headers={"Authorization":f"Bearer {get_created_user_token}"}
+                                    )
+
+    assert response.status_code == 401
+    assert "don\'t have required role" in response.text
+
+
+
+async def test_swap_role_fail_self_admin(client: AsyncClient, get_admin_user_token: str, get_admin_user: User):
+
+    assert get_admin_user.role == UserRoleEnum.ADMIN # default value
+
+    response = await client.patch(url=f"users/{get_admin_user.id}/role",
+                                  json={"new_role": str(UserRoleEnum.PRACTITIONER.value)},
+                                  headers={"Authorization":f"Bearer {get_admin_user_token}"}
+                                    )
+
+    assert response.status_code == 403
+    assert "admin can't change his own role" in response.text
+
+
+
+
+
+
+# ==================================================================================== #
+#                  SWAP STATUS
+# ==================================================================================== #
+
+async def test_swap_status_success(client: AsyncClient, get_admin_user_token:str, get_user_object: User):
+
+    assert get_user_object.status == StatusEnum.ACTIVE # default value
+
+    response = await client.patch(url=f"users/{get_user_object.id}/status",
+                                  json={"new_status": str(StatusEnum.ARCHIVED.value)},
+                                  headers={"Authorization":f"Bearer {get_admin_user_token}"}
+                                    )
+
+    assert response.status_code == 200
+    assert response.json()["status"] == StatusEnum.ARCHIVED.value
+
+
+
+async def test_swap_status_fail_no_admin(client: AsyncClient, get_created_user_token: str, get_user_object: User):
+
+    assert get_user_object.status == StatusEnum.ACTIVE # default value
+
+    response = await client.patch(url=f"users/{get_user_object.id}/status",
+                                  json={"new_status": str(StatusEnum.ARCHIVED.value)},
+                                  headers={"Authorization":f"Bearer {get_created_user_token}"}
+                                    )
+
+    assert response.status_code == 401
+    assert "don\'t have required role" in response.text
+
+
+
+async def test_swap_status_fail_self_admin(client: AsyncClient, get_admin_user_token: str, get_admin_user: User):
+
+    assert get_admin_user.status == StatusEnum.ACTIVE # default value
+
+    response = await client.patch(url=f"users/{get_admin_user.id}/status",
+                                  json={"new_status": str(StatusEnum.ARCHIVED.value)},
+                                  headers={"Authorization":f"Bearer {get_admin_user_token}"}
+                                    )
+
+    assert response.status_code == 403
+    assert "admin can't change his own status" in response.text
 
